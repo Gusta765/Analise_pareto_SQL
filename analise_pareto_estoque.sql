f1a8271eaeb016769e719d2de3c5f06cda655cc0
Gustavo Barbosa <gustavobarbosa7744@gmail.com>
2025-06-04 21:48:21 -0300

    Add files via upload

---
+++ analise_pareto_estoque.sql
@@ -0,0 +1,113 @@
+WITH PARAMETRO AS (
+    SELECT
+        CAST('2021-11-01' AS DATE) AS DATA_INI,
+        CAST('2021-12-31' AS DATE) AS DATA_FIM
+)
+, VENDAS_LIQUIDAS AS ( -- Somando o total liquido de vendas dos úlimos 60 dias
+    SELECT
+        B.produto_id                        AS PRODUTO_ID
+        ,SUM(COALESCE(A.valor_venda, 0.00)) AS TOTAL_LIQUIDO
+    FROM 
+        vendasgerais  AS A
+    INNER JOIN 
+        itensdevendas AS B 
+        ON A.id = B.vendas_id
+    WHERE 
+        CAST(LEFT(A.data, 10) AS DATE) BETWEEN (SELECT DATA_INI FROM PARAMETRO) AND (SELECT DATA_FIM FROM PARAMETRO)
+    GROUP BY 
+        B.produto_id
+)
+, TOTAL_GERAL AS ( -- Total de venda geral
+    SELECT 
+        SUM(total_liquido) AS TOTAL_LIQUIDO_GERAL
+    FROM 
+        VENDAS_LIQUIDAS
+)
+, ORDENADO AS ( -- Ordenando os produtos pela maior venda
+    SELECT 
+        A.PRODUTO_ID                                                     AS PRODUTO_ID
+        ,TOTAL_LIQUIDO                                                   AS TOTAL_LIQUIDO
+        ,ROW_NUMBER() OVER (ORDER BY A.total_liquido DESC, A.produto_id) AS ORDEM
+    FROM 
+        VENDAS_LIQUIDAS AS A
+)
+, PARETO AS ( -- Aplicando o percentual acumulado 
+    SELECT
+        A.PRODUTO_ID                                                                                             AS PRODUTO_ID
+        ,A.TOTAL_LIQUIDO                                                                                         AS TOTAL_LIQUIDO
+        ,B.TOTAL_LIQUIDO_GERAL                                                                                   AS TOTAL_LIQUIDO_GERAL
+        ,SUM(A.TOTAL_LIQUIDO) OVER (ORDER BY A.TOTAL_LIQUIDO DESC, A.PRODUTO_ID)                                 AS ACUMULADO_LIQUIDO
+        ,(SUM(A.TOTAL_LIQUIDO) OVER (ORDER BY A.TOTAL_LIQUIDO DESC, A.PRODUTO_ID) * 100) / B.TOTAL_LIQUIDO_GERAL AS PERC_ACUMULADO
+    FROM 
+        ORDENADO    AS A
+    CROSS JOIN 
+        TOTAL_GERAL AS B
+)
+, PRINPAIS_PRODUTOS AS ( -- Aplicando analise de pareto para descobrir quais são os produtos que trazem maior parte do lucro 
+    SELECT
+        PRODUTO_ID                AS PRODUTO_ID
+        ,TOTAL_LIQUIDO            AS TOTAL_LIQUIDO
+        ,ACUMULADO_LIQUIDO        AS ACUMULADO_LIQUIDO
+        ,ROUND(PERC_ACUMULADO, 2) AS PERC_ACUMULADO
+    FROM 
+        PARETO
+    WHERE
+        PERC_ACUMULADO <= 80.00
+    ORDER BY 
+        TOTAL_LIQUIDO DESC
+)
+, ESTOQUE_PRODUTOS AS ( -- Levantando a quantidade de estoque e Lead Time do fornecedor
+    SELECT 
+        produto_id          AS PRODUTO_ID
+        ,quantidade_estoque AS QUANTIDADE_ESTOQUE
+        ,dias_reposicao     AS DIAS_REPOSICAO
+    FROM 
+        estoque_leadtime
+    WHERE
+        produto_id IN (SELECT PRODUTO_ID FROM PRINPAIS_PRODUTOS)
+)
+, DEMANDA_PRODUTOS AS ( -- Levantando informações da demanda dos produtos nos últimos 60 dias
+    SELECT
+        B.produto_id                AS PRODUTO_ID,
+        SUM(B.item_quantidade) / 60 AS DEMANDA_DIA -- Dividimos pelo número de dias que estamos analisando neste exemplo é 60 dias
+    FROM
+        vendasgerais  AS A
+    JOIN 
+        itensdevendas AS B 
+        ON A.id = B.vendas_id
+    WHERE 
+        B.produto_id IN (SELECT PRODUTO_ID FROM PRINPAIS_PRODUTOS)
+        AND CAST(LEFT(A.data, 10) AS DATE) BETWEEN (SELECT DATA_INI FROM PARAMETRO) AND (SELECT DATA_FIM FROM PARAMETRO)
+    GROUP BY 
+        B.produto_id
+)
+, PEDIDOS_PRODUTOS AS (
+    SELECT 
+        PRODUTO_ID        AS PRODUTO_ID
+        ,STATUS           AS STATUS
+        ,SUM(qtde_pedida) AS QUANTIDADE_PENDENTE 
+    FROM 
+        pedidos_pendentes
+    GROUP BY 
+        PRODUTO_ID     
+)
+SELECT
+    B.PRODUTO_ID                     AS PRODUTO_ID,
+    C.PRODUTO_ID                     AS PROD,
+    QUANTIDADE_ESTOQUE / DEMANDA_DIA AS DIAS_ESTOQUE,
+    DIAS_REPOSICAO                   AS DIAS_REPOSICAO,
+    COALESCE(QUANTIDADE_PENDENTE, 0) AS QUANTIDADE_PENDENTE,
+    CASE 
+        WHEN QUANTIDADE_ESTOQUE / DEMANDA_DIA <= DIAS_REPOSICAO THEN 'Comprar'
+        WHEN C.STATUS = 'Em atraso'                             THEN 'Cobrar Entrega'
+        ELSE 'Ok'
+    END                               AS ACAO
+FROM 
+    DEMANDA_PRODUTOS AS A
+JOIN 
+    ESTOQUE_PRODUTOS AS B
+    ON A.PRODUTO_ID = B.produto_id
+LEFT JOIN 
+    PEDIDOS_PRODUTOS AS C 
+    ON A.PRODUTO_ID = C.PRODUTO_ID
+
